9552ff285052975ea67ee041cbf77060075f23bc
jp-2019 <jeannie.wesell@gmail.com>
2019-04-30 10:48:55 -0400

    Newsletter Form Component

--- angular-techDraft/src/app/app.module.ts
+++ angular-techDraft/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewsletterformComponent } from './newsletterform/newsletterform.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsletterformComponent
   ],
   imports: [
     BrowserModule,
