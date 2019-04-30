213270fb444455b2b88aca1d3313dbba8cc17ba0
shakirullah7892 <shakirullah7892@gmail.com>
2019-04-30 11:32:01 -0400

    [7892] shak's first commit.

--- angular-techDraft/src/app/app.module.ts
+++ angular-techDraft/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShaksCallToActionCompComponent } from './shaks-call-to-action-comp/shaks-call-to-action-comp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShaksCallToActionCompComponent
   ],
   imports: [
     BrowserModule,
