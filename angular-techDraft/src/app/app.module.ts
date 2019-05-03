3add3f70bcaf6ddee108dd251299d23ca3318c44
Rizgar123 <rizgar4321@gmail.com>
2019-05-03 11:29:36 -0500

    hi

--- angular-techDraft/src/app/app.module.ts
+++ angular-techDraft/src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonBoxComponent } from './button-box/button-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonBoxComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
