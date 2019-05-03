4a22a3499496c232eca311bbaa32e9118c4d4c35
Rizgar123 <rizgar4321@gmail.com>
2019-05-03 11:24:12 -0500

    hi every one

---
+++ angular-techDraft/src/app/button-box/button-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button-box',
+  templateUrl: './button-box.component.html',
+  styleUrls: ['./button-box.component.css']
+})
+export class ButtonBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
