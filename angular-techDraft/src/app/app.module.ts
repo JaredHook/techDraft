ae39785fbd70641f6a926bfbdae10cf7d825e87d
Buthaynatune <buthaynakadiri@gmail.com>
2019-05-03 10:26:00 -0400

    my new comp

--- angular-techDraft/src/app/app.module.ts
+++ angular-techDraft/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GgithabButtonComponent } from './ggithab-button/ggithab-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GgithabButtonComponent
   ],
   imports: [
     BrowserModule,
