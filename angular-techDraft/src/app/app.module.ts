6c2f07a49a755cd99979efac927dabfefbbc9905
jp-2019 <jeannie.wesell@gmail.com>
2019-05-03 09:09:50 -0400

    [12345] Riz's contact card component

--- angular-techDraft/src/app/app.module.ts
+++ angular-techDraft/src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewsletterformComponent } from './newsletterform/newsletterform.component';
-
+import { ContentCardComponent } from './content-card/content-card.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NewsletterformComponent
+    NewsletterformComponent,
+    ContentCardComponent,
   ],
   imports: [
     BrowserModule,
