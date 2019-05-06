cc190b49703db0e85aab5be6879d0fcac14f5ce6
shakirullah7892 <shakirullah7892@gmail.com>
2019-05-06 08:23:16 -0400

    [7892] commit

--- angular-techDraft/src/app/app.module.ts
+++ angular-techDraft/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShaksCallToActionCompComponent } from './shaks-call-to-action-comp/shaks-call-to-action-comp.component';
+import { NewComponent } from './new_component/new.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShaksCallToActionCompComponent
+    ShaksCallToActionCompComponent,
+    NewComponent
   ],
   imports: [
     BrowserModule,
