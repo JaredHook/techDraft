44ffa25ae142ff601d77b1ff6825190d6dbc07fd
Jared Hook <jared_hook@hotmail.com>
2019-05-06 08:52:14 -0400

    rebuilt footer component as "bottom"

--- angular-techDraft/src/app/app.module.ts
+++ angular-techDraft/src/app/app.module.ts
@@ -4,24 +4,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewsletterformComponent } from './newsletterform/newsletterform.component';
 import { TopNavBarComponent } from './top-nav-bar/top-nav-bar.component';
-import { FooterComponent } from './footer/footer.component';
 import { GgithabButtonComponent } from './ggithab-button/ggithab-button.component';
 import { ShaksCallToActionCompComponent } from './shaks-call-to-action-comp/shaks-call-to-action-comp.component';
+import { BottomComponent } from './bottom/bottom.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GgithabButtonComponent
+    GgithabButtonComponent,
     ShaksCallToActionCompComponent,
     AppComponent,
-    NewsletterformComponent
+    NewsletterformComponent,
     TopNavBarComponent,
-    FooterComponent
+    BottomComponent,
   ],
    
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
