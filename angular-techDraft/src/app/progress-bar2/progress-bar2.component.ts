2cbec039dcc3d9d4c1abf1cbf18e2f08852ee832
Kenny Imarah <kenny.o.imarah@gmail.com>
2019-05-21 11:43:28 -0400

    Updated progress bar

---
+++ angular-techDraft/src/app/progress-bar2/progress-bar2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-progress-bar2',
+  templateUrl: './progress-bar2.component.html',
+  styleUrls: ['./progress-bar2.component.css']
+})
+export class ProgressBar2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
