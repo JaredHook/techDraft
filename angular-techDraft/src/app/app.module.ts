dbc79e2997a3b01846750f6fe5a3bd7766c2be2d
gregorywhytepatriots <gregorywhyte@rogers.com>
2019-05-21 10:02:52 -0400

    updatecode

--- angular-techDraft/src/app/app.module.ts
+++ angular-techDraft/src/app/app.module.ts
@@ -9,10 +9,21 @@ import { GgithabButtonComponent } from './ggithab-button/ggithab-button.componen
 import { ShaksCallToActionCompComponent } from './shaks-call-to-action-comp/shaks-call-to-action-comp.component';
 import { BottomComponent } from './bottom/bottom.component';
 import { ThemeComponent } from './theme/theme.component';
-import { TeamTableComponent } from './team-table/team-table.component';
-import { MyNavComponent } from './my-nav/my-nav.component';
+// import { TeamTableComponent } from './team-table/team-table.component';
+//import { MyNavComponent } from './my-nav/my-nav.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { Routes, RouterModule } from '@angular/router';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { EditProfileCompComponent } from './edit-profile-comp/edit-profile-comp.component';
+import { TabComponent } from './tab/tab.component';
+import { TeamTableComponent } from './team-table/team-table.component';
 
+
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'userProfile', component: UserProfileComponent}
+
+];
 
 @NgModule({
   declarations: [
@@ -26,13 +37,17 @@ import { HomePageComponent } from './home-page/home-page.component';
     BottomComponent,
     ThemeComponent,
     TeamTableComponent,
-    MyNavComponent,
+   // MyNavComponent,
     HomePageComponent,
+    UserProfileComponent,
+    EditProfileCompComponent,
+    TabComponent,
+    TeamTableComponent
   ],
    
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    RouterModule.forRoot(routes) 
   ],
   providers: [],
   bootstrap: [AppComponent]
