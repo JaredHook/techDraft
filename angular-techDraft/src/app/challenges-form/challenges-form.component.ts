1018f57d2d342ace8be25ed2a07a2da672bb6004
jp-2019 <jeannie.wesell@gmail.com>
2019-06-02 16:33:04 -0400

    first commit of changes to challenges tab

---
+++ angular-techDraft/src/app/challenges-form/challenges-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-challenges-form',
+  templateUrl: './challenges-form.component.html',
+  styleUrls: ['./challenges-form.component.css']
+})
+export class ChallengesFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
